59df9616018cefcc391e2a771e2f0d98fde77451
Keon White <keon-1@hotmail.co.uk>
2023-03-21 17:59:29 +0000

    Sign up page CSS restyle

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,142 +1,159 @@
 import React from "react";
 import spotify from "../assets/login_assets/spotify.png";
-import fbimg00 from "../assets/signup_assets/fbimg00.png";
 import googleimg00 from "../assets/signup_assets/googleimg00.png";
-import "../styles/signUp.css";
-// import FacebookIcon from "@mui/icons-material/Facebook";
+import "../styles/SignUp.css";
 import { useNavigate, Link } from "react-router-dom";
+import FacebookIcon from "@mui/icons-material/Facebook";
 
 export default function ContainerSignUp() {
   const navigate = useNavigate();
   return (
-    <div className="body">
-      <div className="part1">
-        <img className="img" src={spotify} alt="" />
-      </div>
+    <div className="signup--container">
+      <div className="signup--layout">
 
-      <div className="part2">
-        <h1 className="header"> Sign up for free to start listening.</h1>
+        <div className="signup--top-section">
+          <img className="signup--logo" src={spotify} alt="" />
+          <h1 className="signup--header"> Sign up for free to start listening.</h1>
+
+          <div className="signup--btns">
+            <button className="signup--fb-btn signup--btn"><FacebookIcon className="signup--fb-icon"/>Sign up with Facebook</button>
+            <button className="signup--google-btn signup--btn">
+              <img className="signup--img-google" src={googleimg00} alt="" />
+              Sign up with Google
+            </button>
+          </div>
+        </div>
+
+        <div className="signup--divider">
+          <hr className="signup--hr"/>
+          <p className="signup--or">or</p>
+          <hr className="signup--hr"/>
+        </div>
+
+        <form className="signup--form">
+          <h3 className="signup--form-title">Sign up with your email address</h3>
+
+          <label htmlFor="email" className="label">What is your email?</label>
+          <input id="email" className="input" type="email" placeholder="Enter your email"/>
+          {/* <br /> */}
+          <label htmlFor="confirm" className="label">Confirm your email</label>
+          <input id="confirm" className="input" type="text" placeholder="Confirm your email again"/>
+
+          <label htmlFor="password" className="label">Create your password</label>
+          <input id="password" className="input" type="password" placeholder="Create a Password"/>
+
+          <label htmlFor="name" className="label">What should we call you?</label>
+          <input id="name" className="input" type="text" placeholder="Enter a Profile name"/>
+          <h6 className="signup--form-name-small">This appears on your profile.</h6>
+
+          <p className="label">What's your date of birth?</p>
+          <div className="signup--date-container">
+            <div className="signup--date-wrappers">
+              <label for="day" className="signup--form-date-labels">Day</label>
+              <input id="day" className="signup--form-day-input input-date" type="text" placeholder="DD" />
+            </div>
+
+            <div className="signup--date-wrappers">
+              <label for="month" className="signup--form-date-labels">Month</label>
+              <select id="month" className="signup--form-month-input input-date">
+                <option>Month</option>
+                <option>January</option>
+                <option>February</option>
+                <option>March</option>
+                <option>April</option>
+                <option>May</option>
+                <option>June</option>
+                <option>July</option>
+                <option>August</option>
+                <option>September</option>
+                <option>October</option>
+                <option>November</option>
+                <option>December</option>
+              </select>
+            </div>
+
+              <div className="signup--date-wrappers">
+                <label for="year" className="signup--form-date-labels">Year</label>
+                <input id="year" className="signup--form-year-input input-date" type="text" placeholder="YYYY" />
+              </div>
+          </div>
+
+          <p className="label">What's your gender?</p>
+          <div className="signup--gender-wrapper">
+            <input name="signup-radio" id="male" className="input-radio" type="radio" />
+            <label htmlFor="male" className="label-radio">Male</label>
+            <input name="signup-radio" id="female" className="input-radio" type="radio" />
+            <label htmlFor="female" className="label-radio">Female</label>
+            <input name="signup-radio" id="nonbinary" className="input-radio" type="radio" />
+            <label htmlFor="nonbinary" className="label-radio">Non-binary</label>
+            <input name="signup-radio" id="other" className="input-radio" type="radio" />
+            <label htmlFor="other" className="label-radio">Other</label>
+            <br />
+            <input name="signup-radio" id="prefer" className="input-radio radio-prefer" type="radio" />
+            <label htmlFor="prefer" className="label-radio">Prefer not to say</label>
+          </div>
+
+          <div className="signup--checkbox-container">
+            <div className="signup--checkbox-wrapper">
+              <input id="marketing" className="signup--checkbox" type="checkbox" />
+              <label htmlFor="marketing" className="signup--checkbox-label">
+                I would prefer not to receive marketing messages from Spotify
+              </label>
+            </div>
+            <br/>
+            <div className="signup--checkbox-wrapper">
+              <input id="data-sharing" className="signup--checkbox" type="checkbox" />
+              <label htmlFor="data-sharing" className="signup--checkbox-label">
+                Share my registration data with Spotify's content providers for
+                marketing purposes. Note that your data may be transferred to a
+                country outside of the EEA as <br /> described in our privacy policy.
+              </label>
+            </div>
+          </div>
+        </form>
+
+        <h5 className="signup--tc">
+          Spotify is a personalised service. By clicking on sign-up, you agree to
+          Spotify's&nbsp;
+          <Link
+            className="signup--link"
+            href="http://"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Terms and Conditions of Use.
+          </Link>
+        </h5>
+
+        <h5 className="signup--tc">
+          To learn more about how Spotify collects, uses, shares and protects your
+          personal data, please see&nbsp;
+          <Link
+            className="signup--link"
+            href="http://"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Spotify's Privacy Policy.
+          </Link>
+        </h5>
+
+        <button onClick={() => navigate('')} className="signup--button">
+          Sign Up
+        </button>
+
+        <h3 className="signup--account-q">
+          Have an account?{" "}
+          <Link
+            to="/login"
+            className="signup--link"
+            href="http://"
+            rel="noopener noreferrer"
+          >
+            Log in
+          </Link>
+        </h3>
       </div>
-      <div className="part3">
-        <button className="fbbtn">Sign up with Facebook</button>
-        <img className="spanImgfb" src={fbimg00} alt="" />
-        <button className="googlebtn">Sign up with Google</button>
-        <img className="spanImggoogle" src={googleimg00} alt="" />
-      </div>
-      <form className="form">
-        <div id="span1"></div> <h4 className="or">or</h4>
-        <div id="span2"></div>
-        <h3 className="signUp">Sign up with your email address</h3>
-        <label className="emailLabel">What is your email?</label>
-        <input
-          className="emailInput"
-          type="text"
-          placeholder="Enter your email"
-        />
-        <br />
-        <label className="emailLabel1">Confirm your email</label>
-        <input
-          className="emailInput1"
-          type="text"
-          placeholder="Confirm your email again"
-        />
-        <label className="passwordLabel">Create your password</label>
-        <input
-          className="passwordInput"
-          type="password"
-          placeholder="Create a Password"
-        />
-        <label className="profileLabel">What should we call you?</label>
-        <input
-          className="profileInput"
-          type="text"
-          placeholder="Enter a Profile name"
-        />
-        <h6 className="small">This appears on your profile.</h6>
-        <h4 className="dob">What's your date of birth?</h4>
-        <label className="dd">Day</label>
-        <br />
-        <input className="day" type="text" placeholder="DD" />
-        <label className="month">Month</label>
-        <select className="months">
-          <option>Month</option>
-          <option>January</option>
-          <option>February</option>
-          <option>March</option>
-          <option>April</option>
-          <option>May</option>
-          <option>June</option>
-          <option>July</option>
-          <option>August</option>
-          <option>September</option>
-          <option>October</option>
-          <option>November</option>
-          <option>December</option>
-        </select>
-        <label className="yearLabel">Year</label>
-        <input className="yearInput" type="text" placeholder="YYYY" />
-        <h4 className="gender">What's your gender?</h4>
-        <input className="maleRadio" type="radio" />
-        <label className="male">Male</label>
-        <input className="femaleRadio" type="radio" />
-        <label className="female">Female</label>
-        <input className="nonbinaryRadio" type="radio" />
-        <label className="nonbinary">Non-binary</label>
-        <input className="otherRadio" type="radio" />
-        <label className="other">Other</label>
-        <br />
-        <input className="preferRadio" type="radio" />
-        <label className="prefer">Prefer not to say</label>
-        <input className="checkbox0" type="checkbox" />
-        <label className="labelmessage">
-          I would prefer not to receive marketing messages from Spotify
-        </label>
-        <input className="checkbox1" type="checkbox" />
-        <label className="labelmessage0">
-          Share my registration data with Spotify's content providers for
-          marketing purposes. Note that your data may be transferred to a
-          country outside of the EEA as <br /> described in our privacy policy.
-        </label>
-      </form>
-      <h5 className="spot">
-        Spotify is a personalised service. By clicking on sign-up, you agree to
-        Spotify's
-        <a
-          className="a"
-          href="http://"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Terms and Conditions of Use.
-        </a>
-      </h5>
-      <h5 className="learn">
-        To learn more about how Spotify collects, uses, shares and protects your
-        personal data, <br /> please see
-        <a
-          className="a"
-          href="http://"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Spotify's Privacy Policy.
-        </a>
-      </h5>
-      <button onClick={() => navigate("")} className="button">
-        Sign Up
-      </button>
-      <h3 className="h3">
-        Have an account?{" "}
-        <Link
-          to="/signup"
-          className="a"
-          href="http://"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Log in
-        </Link>
-      </h3>
     </div>
   );
 }
---
+++ src/reference/SignUpOld.js
@@ -0,0 +1,148 @@
+import React from "react";
+import spotify from "../assets/login_assets/spotify.png";
+import fbimg00 from "../assets/signup_assets/fbimg00.png";
+import googleimg00 from "../assets/signup_assets/googleimg00.png";
+// import "../styles/signUpOld.css";
+// import FacebookIcon from "@mui/icons-material/Facebook";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function SignUp() {
+  const navigate = useNavigate();
+  return (
+    <div className="body">
+      <div className="part1">
+        <img className="img" src={spotify} alt="" />
+      </div>
+
+      <div className="part2">
+        <h1 className="header"> Sign up for free to start listening.</h1>
+      </div>
+
+      {/* <div className="part3">
+        <button className="fbbtn">Sign up with Facebook</button>
+        <img className="spanImgfb" src={fbimg00} alt="" />
+        <button className="googlebtn">Sign up with Google</button>
+        <img className="spanImggoogle" src={googleimg00} alt="" />
+      </div> */}
+
+      {/* <form className="form">
+        <div id="span1"></div> <h4 className="or">or</h4>
+        <div id="span2"></div>
+        <h3 className="signUp">Sign up with your email address</h3>
+        <label className="emailLabel">What is your email?</label>
+        <input
+          className="emailInput"
+          type="text"
+          placeholder="Enter your email"
+        />
+        <br />
+        <label className="emailLabel1">Confirm your email</label>
+        <input
+          className="emailInput1"
+          type="text"
+          placeholder="Confirm your email again"
+        />
+        <label className="passwordLabel">Create your password</label>
+        <input
+          className="passwordInput"
+          type="password"
+          placeholder="Create a Password"
+        />
+        <label className="profileLabel">What should we call you?</label>
+        <input
+          className="profileInput"
+          type="text"
+          placeholder="Enter a Profile name"
+        />
+        <h6 className="small">This appears on your profile.</h6>
+        <h4 className="dob">What's your date of birth?</h4>
+        <label className="dd">Day</label>
+        <br />
+        <input className="day" type="text" placeholder="DD" />
+        <label className="month">Month</label>
+        <select className="months">
+          <option>Month</option>
+          <option>January</option>
+          <option>February</option>
+          <option>March</option>
+          <option>April</option>
+          <option>May</option>
+          <option>June</option>
+          <option>July</option>
+          <option>August</option>
+          <option>September</option>
+          <option>October</option>
+          <option>November</option>
+          <option>December</option>
+        </select>
+        <label className="yearLabel">Year</label>
+        <input className="yearInput" type="text" placeholder="YYYY" />
+        <h4 className="gender">What's your gender?</h4>
+        <input className="maleRadio" type="radio" />
+        <label className="male">Male</label>
+        <input className="femaleRadio" type="radio" />
+        <label className="female">Female</label>
+        <input className="nonbinaryRadio" type="radio" />
+        <label className="nonbinary">Non-binary</label>
+        <input className="otherRadio" type="radio" />
+        <label className="other">Other</label>
+        <br />
+        <input className="preferRadio" type="radio" />
+        <label className="prefer">Prefer not to say</label>
+        <input className="checkbox0" type="checkbox" />
+        <label className="labelmessage">
+          I would prefer not to receive marketing messages from Spotify
+        </label>
+        <input className="checkbox1" type="checkbox" />
+        <label className="labelmessage0">
+          Share my registration data with Spotify's content providers for
+          marketing purposes. Note that your data may be transferred to a
+          country outside of the EEA as <br /> described in our privacy policy.
+        </label>
+      </form> */}
+
+      {/* <h5 className="spot">
+        Spotify is a personalised service. By clicking on sign-up, you agree to
+        Spotify's
+        <a
+          className="a"
+          href="http://"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Terms and Conditions of Use.
+        </a>
+      </h5> */}
+
+      {/* <h5 className="learn">
+        To learn more about how Spotify collects, uses, shares and protects your
+        personal data, <br /> please see
+        <a
+          className="a"
+          href="http://"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Spotify's Privacy Policy.
+        </a>
+      </h5> */}
+
+      {/* <button onClick={() => navigate("Main")} className="button">
+        Sign Up
+      </button> */}
+
+      {/* <h3 className="h3">
+        Have an account?{" "}
+        <Link
+          to="/signup"
+          className="a"
+          href="http://"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Log in
+        </Link>
+      </h3> */}
+    </div>
+  );
+}
